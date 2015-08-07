3965b77a4d950289c1bde4d84e37b845248a679a
Ben N. <fleexy@mchsi.com>
2015-08-07 17:21:54 -0500

    One-at-a-time loading

--- OpenThar.js
+++ OpenThar.js
@@ -13,9 +13,25 @@ function resetResources() {
     maphead = undefined;
     backtils = undefined;
     foretils = undefined;
+    document.getElementById("resGamemaps").innerText = "";
+    document.getElementById("resMaphead").innerText = "";
+    document.getElementById("resBacktils").innerText = "";
+    document.getElementById("resForetils").innerText = "";
+    document.getElementById("goEasy").style.display = "inline";
+    document.getElementById("goHard").style.display = "inline";
+    document.getElementById("goFinal").style.display = "none";
+}
+function setShowPickLinks(show) {
+    var display = show ? "inline" : "none";
+    document.getElementById("pickGamemaps").style.display = display;
+    document.getElementById("pickMaphead").style.display = display;
+    document.getElementById("pickBacktils").style.display = display;
+    document.getElementById("pickForetils").style.display = display;
 }
 function setupEasy() {
     resetResources();
+    setShowPickLinks(false);
+    document.getElementById("resTable").style.display = "none";
     var options = {
         success: function(files) {
             if (files.length == 4) {
@@ -49,9 +65,69 @@ function setupEasy() {
                 setupResponse("Oops", "You need to select all four files (main maps file, map header, background tileset, and foreground tileset). Try again, or use the other option to select one file at once.");
             }
         },
-        cancel: function() {},
+        cancel: function() {
+            resetResources();
+        },
         linkType: "direct",
         multiselect: true
     }
+    setupResponse("Choosing files", "A Dropbox window should have opened so you can choose your files. If nothing happened, you might have an overzealous pop-up blocker.")
     Dropbox.choose(options);
+}
+function setupHard() {
+    resetResources();
+    setShowPickLinks(true);
+    document.getElementById("resTable").style.display = "table";
+    document.getElementById("goHard").style.display = "none";
+    document.getElementById("goFinal").style.display = "inline";
+    setupResponse("Choose your files", "Click Pick next to each resource type to browse your Dropbox for that resource, then click Launch Editor when finished.");
+}
+function pickGamemaps() {
+    Dropbox.choose({
+        success: function(files) {
+            gamemaps = files[0];
+            document.getElementById("resGamemaps").innerText = gamemaps.name;
+        },
+        cancel: function() {},
+        linkType: "direct",
+        multiselect: false
+    });
+}
+function pickMaphead() {
+    Dropbox.choose({
+        success: function(files) {
+            maphead = files[0];
+            document.getElementById("resMaphead").innerText = maphead.name;
+        },
+        cancel: function() {},
+        linkType: "direct",
+        multiselect: false
+    });
+}
+function pickBacktils() {
+    Dropbox.choose({
+        success: function(files) {
+            backtils = files[0];
+            document.getElementById("resBacktils").innerText = backtils.name;
+        },
+        cancel: function() {},
+        linkType: "direct",
+        multiselect: false
+    });
+}
+function pickForetils() {
+    Dropbox.choose({
+        success: function (files) {
+            foretils = files[0];
+            document.getElementById("resForetils").innerText = foretils.name;
+        },
+        cancel: function() {},
+        linkType: "direct",
+        multiselect: false
+    });
+}
+function confirmSetup() {
+    if (gamemaps !== undefined && maphead !== undefined && backtils !== undefined && foretils !== undefined) {
+        // TODO: Actual editor setup and canvas stuff
+    }
 }
